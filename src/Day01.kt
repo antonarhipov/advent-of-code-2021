17d9d51ad6d73c3fe349afff730e9b0ad8bafe21
anton <ant.arhipov@gmail.com>
2021-12-02 11:02:26 +0200

    Solution for Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,18 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun part1(input: List<Int>): Int {
+        return input.windowed(2).count { (a, b) -> a < b }
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    // A + B + C <=> B + C + D
+    fun part2(input: List<Int>): Int {
+        return input
+            .windowed(4)
+            .count {
+                it[0] < it[3]
+            }
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    val input = readInput("Day01")
+    val input = readInputAsInts("Day01")
     println(part1(input))
     println(part2(input))
 }
--- src/Utils.kt
+++ src/Utils.kt
@@ -7,6 +7,9 @@ import java.security.MessageDigest
  */
 fun readInput(name: String) = File("src", "$name.txt").readLines()
 
+
+fun readInputAsInts(name: String) = File("src", "$name.txt").readLines().map { it.toInt() }
+
 /**
  * Converts string to md5 hash.
  */
